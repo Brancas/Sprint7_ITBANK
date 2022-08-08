6b18ef7416e8c485c34504c9bfd569c144885275
Leonel Baltazar Bravo <leonelbbravo@gmail.com>
2022-08-08 19:43:32 -0300

    apps

--- HOMEBANKING/HOMEBANKING/settings.py
+++ HOMEBANKING/HOMEBANKING/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Cliente',
+    'Cuenta',
+    'Login',
+    'Prestamo',
+    'Tarjeta',
 ]
 
 MIDDLEWARE = [
