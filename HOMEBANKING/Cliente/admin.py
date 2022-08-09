f6492ba4f932951d6e8cc98a2ec00e60d6b18804
CplusArd <sebakpo120@gmail.com>
2022-08-09 10:27:41 -0300

    Tabla Prestamos y edicion del djangoadministration

--- HOMEBANKING/Cliente/admin.py
+++ HOMEBANKING/Cliente/admin.py
@@ -3,6 +3,12 @@ from Cliente import models
 
 # Register your models here.
 
+class Cliente(admin.ModelAdmin):
+    list_display = ['id', 'nombre', 'DNI', "FechaNacimiento", "Telefono", "Email"]
+    list_filter = ['id', 'nombre', 'DNI', "FechaNacimiento", "Telefono", "Email"]
+    search_fields = ['id', 'nombre', 'DNI', "FechaNacimiento", "Telefono", "Email"]
+    
+
 admin.site.register(models.Cliente)
 admin.site.register(models.Empleado)
 
--- HOMEBANKING/Cliente/models.py
+++ HOMEBANKING/Cliente/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+
 class Cliente(models.Model):
     Apellido = models.CharField(max_length=30, verbose_name='Apellido')
     Nombre = models.CharField(max_length=30, verbose_name='Nombre')
-    DNI = models.IntegerField( verbose_name='DNI')
+    DNI = models.IntegerField(verbose_name='DNI')
     FechaNacimiento = models.DateField(verbose_name='Fecha Nacimiento')
     telefono = models.IntegerField(verbose_name='telefono')
     Email = models.EmailField(max_length=255, verbose_name='Email')
@@ -13,13 +14,24 @@ class Cliente(models.Model):
     def __str__(self):
         return self.Apellido
 
+
 class Empleado(models.Model):
     Apellido = models.CharField(max_length=30, verbose_name='Apellido')
     Nombre = models.CharField(max_length=30, verbose_name='Nombre')
-    DNI = models.IntegerField( verbose_name='DNI')
+    DNI = models.IntegerField(verbose_name='DNI')
     FechaNacimiento = models.DateField(verbose_name='Fecha Nacimiento')
     telefono = models.IntegerField(verbose_name='telefono')
     Email = models.EmailField(max_length=255, verbose_name='Email')
+
+    def __str__(self):
+        return self.Apellido
+
+
+class Prestamo(models.Model):
+    Tipo = models.CharField(max_length=255, verbose_name="Tipo")
+    Fecha = models.DateField(auto_now_add=True, verbose_name="Fecha")
+    Total = models.IntegerField(verbose_name="Total")
+    # cliente id foring key?
 
     def __str__(self):
         return self.Apellido
