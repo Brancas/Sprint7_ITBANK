426aba4d0303638a705940b68fac493cc4239e11
CplusArd <35538199+CplusArd@users.noreply.github.com>
2022-08-09 06:52:25 -0300

    Creacion Tabla "Cliente"

--- HOMEBANKING/Cliente/models.py
+++ HOMEBANKING/Cliente/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Cliente(models.Model):
+    Apellido = models.CharField(max_length=30, verbose_name='Apellido')
+    Nombre = models.CharField(max_length=30, verbose_name='Nombre')
+    DNI = models.IntegerField( verbose_name='DNI')
+    FechaNacimiento = models.DateField(verbose_name='Fecha Nacimiento')
+    telefono = models.IntegerField(verbose_name='telefono')
+    Email = models.EmailField(max_length=255, verbose_name='Email')
+
+    def __str__(self):
+        return self.Apellido
