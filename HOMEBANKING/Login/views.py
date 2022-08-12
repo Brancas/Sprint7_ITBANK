1d435078680cdaaa6322fe23a9c7a8049684acad
Lucasoftware7 <94875496+Lucasoftware7@users.noreply.github.com>
2022-08-12 16:33:25 -0300

    continuando con registro

--- HOMEBANKING/Login/views.py
+++ HOMEBANKING/Login/views.py
@@ -1,11 +1,32 @@
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render,redirect
 from django.template import Template,context
 from django.template import loader
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from .forms import registroForm
+
 # Create your views here.
 admin_chango="http://127.0.0.1:8000/admin/"
+
+def registro_usuario(request):
+    submitted=False
+    if request.method == 'POST':
+        form=registroForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/registro_usuario?submmited=True')
+    else:
+        form=registroForm
+        if 'submitted' in request.GET:
+            submitted=True
+             
+
+    return render(request,'registro.html',{'form':form,'submitted':submitted})
+
+
+
 def login_usuario(request):
     # Con este if comprobamos que el usuario haya rellenado los formularios.
     # Si no los rellena solamente muestra la pagina.        
