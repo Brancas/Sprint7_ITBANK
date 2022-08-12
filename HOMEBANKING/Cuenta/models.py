6d2e441d1aabbc405340bb1d5d258d8d806a5241
CplusArd <35538199+CplusArd@users.noreply.github.com>
2022-08-12 07:09:19 -0300

    tabla prestamo

--- HOMEBANKING/Cuenta/models.py
+++ HOMEBANKING/Cuenta/models.py
@@ -4,9 +4,17 @@ from Cliente.models import Cliente
 
 # Create your models here.
 
+
 class Cuenta(models.Model):
-    cliente = models.ForeignKey(Cliente, on_delete = models.CASCADE)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
     balance = models.FloatField(verbose_name="balance")
     iban = models.CharField(verbose_name="iban", max_length=255)
-    tipo_cuenta = models.CharField(choices= tipo_cuentas, max_length=3)
+    tipo_cuenta = models.CharField(choices=tipo_cuentas, max_length=3)
 
+    class Meta:
+        verbose_name = 'Cuenta'
+        verbose_name_plural = 'Cuentas'
+        db_table = 'Cuenta'
+
+    def __str__(self):
+        return self.cliente
--- HOMEBANKING/Prestamo/models.py
+++ HOMEBANKING/Prestamo/models.py
@@ -1,12 +1,14 @@
 from django.db import models
+from Cliente.models import Cliente
+from .choices import tipo_prestamos
 
 # Create your models here.
 
 class Prestamo(models.Model):
-    Tipo = models.CharField(max_length=255, verbose_name="Tipo")
+    Tipo = models.CharField(choices = tipo_prestamos, max_length= 3, default= 'HTO')
     Fecha = models.DateField(auto_now_add=True, verbose_name="Fecha")
     Total = models.IntegerField(verbose_name="Total")
-    # cliente id foring key?
+    Cliente = models.ForeignKey(Cliente,null= True, blank= True, on_delete =  models.CASCADE)
 
     def __str__(self):
         return self.Tipo
