3e937c8e1bb7a60027c7d09e5aa92763e2820c2a
CplusArd <35538199+CplusArd@users.noreply.github.com>
2022-08-12 19:35:58 -0300

    tarjeta

--- HOMEBANKING/Tarjeta/models.py
+++ HOMEBANKING/Tarjeta/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Tarjeta(models.Model):
+    numero_tarjeta = models.IntegerField(max_length=20 ,verbose_name="numero_tarjeta")
+    cvv = models.IntegerField(max_length=4 ,verbose_name= "cvv")
+    fecha = models.DateField(auto_now_add=True, verbose_name="fecha")
+    fecha_expiracion = models.DateField(verbose_name="fecha_expiracion")
