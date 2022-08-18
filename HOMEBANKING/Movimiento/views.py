04a610f46632312ea3f803906518ba0edb1d709d
Sebastian Prieto <35538199+SebaGPDev@users.noreply.github.com>
2022-08-18 09:58:45 -0300

    Revert "Merge branch 'main' into sebastian"
    This reverts commit b0d888a81221dc2a9b81d5cc39689a94addd5bff, reversing
    changes made to aa8eac8a70b8b8a085571e3a37daeb6387aee5f4.

--- HOMEBANKING/Movimiento/views.py
+++ HOMEBANKING/Movimiento/views.py
@@ -1,19 +1,10 @@
 from django.shortcuts import render
 from .forms import TransferenciaForm
-from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib import messages
-from HOMEBANKING.forms  import ExtendUserCreationForm, UserProfileForm
 
 
 # Create your views here.
-def main_movimientos(request):
 
-    return render(request, 'main_movimientos.html')
-
-
-def main_transferencia(request):
+def transferencia(request):
     """Esta funcion recibe la cuenta destino y el monto a transferir
     ! Falta que se conecte con la base de datos y modifique los campos correspondientes.
     ! Falta que autentifique el usuario y tome sus datos"""
@@ -26,5 +17,4 @@ def main_transferencia(request):
         if OBJ_transForm.is_valid():
             CNT_destino = request.POST.get('CT_Destino', '')
             TRF_monto = request.POST.get('TRF_monto', '')   
-    
-    return render(request, 'main_transferencia.html', {'form': OBJ_transForm})
+    return render(request, 'form.html', {'form': OBJ_transForm})
--- HOMEBANKING/Prestamo/views.py
+++ HOMEBANKING/Prestamo/views.py
@@ -1,13 +1,3 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib import messages
-from HOMEBANKING.forms  import ExtendUserCreationForm, UserProfileForm
 
 # Create your views here.
-
-
-def main_prestamos(request):
-
-    return render(request, 'main_prestamos.html')
